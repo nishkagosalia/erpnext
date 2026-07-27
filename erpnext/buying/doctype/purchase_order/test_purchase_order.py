a628d41d702006b2a372091d7bbc0ecf2607ba92
nishkagosalia <nishka.gosalia@gmail.com>
2026-07-27 14:18:10 +0530

    fix: handling negative total for po and so

--- erpnext/buying/doctype/purchase_order/test_purchase_order.py
+++ erpnext/buying/doctype/purchase_order/test_purchase_order.py
@@ -54,6 +54,21 @@ class TestPurchaseOrder(ERPNextTestSuite):
 		po.save()
 		self.assertEqual(po.items[1].qty, 1)
 
+	def test_purchase_order_negative_grand_total_blocked_by_default(self):
+		po = create_purchase_order(qty=1, rate=100, do_not_save=True)
+		po.apply_discount_on = "Grand Total"
+		po.additional_discount_amount = 150
+		self.assertRaises(frappe.ValidationError, po.save)
+
+	@ERPNextTestSuite.change_settings("Buying Settings", {"allow_negative_rates_for_items": 1})
+	def test_purchase_order_negative_grand_total_allowed_with_setting(self):
+		"""A supplier change order can net to a negative grand total (credit owed)."""
+		po = create_purchase_order(qty=1, rate=100, do_not_save=True)
+		po.apply_discount_on = "Grand Total"
+		po.additional_discount_amount = 150
+		po.save()
+		self.assertTrue(po.base_grand_total < 0)
+
 	def test_purchase_order_zero_qty(self):
 		po = create_purchase_order(qty=0, do_not_save=True)
 
